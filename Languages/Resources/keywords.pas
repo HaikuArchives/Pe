b72c7bfc9f39304c6d7598348c4a04e663914d81
ksherlock <ksherlock@nowhere.fake>
2003-03-08 17:08:10 +0000

    Keywords for the Pascal language plugin.
    --HG--
    extra : convert_revision : svn%3Acdae58ac-7104-0410-addd-a5e18705bff5/trunk%4019

---
+++ Languages/Resources/keywords.pas
@@ -0,0 +1,12 @@
+and array begin case const div do downto else end file for forward
+function goto if implementation in inherited inline interface label
+mod not object of or otherwise override packed procedure program
+record repeat set then to type unit univ until uses var while with
+true false nil  
+- User set 1:
+char byte integer longint boolean real double extended comp string
+- User set 2:
+abs arctan chr cos dispose eof eoln exp get ln new odd ord pack page pred put
+read readln round sin sqr sqrt succ trunc unpack write writeln
+- User set 3:
+- User set 4:
