ddddf93dee32e5596384ec284be7fcf04714a6dd
bipolar <bipolar@nowhere.fake>
2004-03-17 06:48:36 +0000

    implemented results popup for BeBook extension.
    --HG--
    extra : convert_revision : svn%3Acdae58ac-7104-0410-addd-a5e18705bff5/trunk%4093

--- Extensions/BeBookFetch.cpp
+++ Extensions/BeBookFetch.cpp
@@ -1,30 +1,32 @@
 //-----------------------------------------------------------------------------
-// Ashamelessly stolen from Alan Ellis's Be API Fetch for Eddie.
+// BeBookFetch: looks up the selected text again BeBook's doc bookmarks.
+//
+// Contains (legally) stolen code from:
+//  - Alan Ellis's Be API Fetch for Eddie (http://www.bebits.com/app/6).
 //-----------------------------------------------------------------------------
-
-/*
-This is an extension for Pe. It allows you to search the BeBook for the current
-selection (as BeIDE does).
-I used Alan Ellis's Be API Fetch (http://www.bebits.com/app/6) as a base (yeah,
-riiight!). Ok, I stole most of his code, but he allow us to do so (phew!) :-)
-
-In the current encarnation you only get displayed the first result of the query.
-This means that you don't get a window with the results (like BeIDE has).
-Anyway... its handy.
-*/
 
 #include <vector>
 #include <ctype.h>
 
 #include <Alert.h>
+#include <MenuItem.h>
 #include <Path.h>
+#include <PopUpMenu.h>
 #include <Query.h>
 #include <Roster.h>
 #include <String.h>
 #include <Volume.h>
 #include <VolumeRoster.h>
 
+#include <typeinfo> // otherwise I get an error in PDoc.h :-/
+#include <Debug.h>  // ditto.
+#include "PDoc.h"
+
 #include "MTextAddOn.h"
+#include "HButtonBar.h"
+#include "PToolBar.h"
+
+//------------------------------------------------------------------------------
 
 #define kBeTypeAttrib  "BEOS:TYPE"
 #define kIndexFileType "application/x-vnd.Be-doc_bookmark"
@@ -35,6 +37,11 @@ Anyway... its handy.
 #define kAlertName     "Pe's BeAPI Fetcher Alert"
 #define kBtnText       "Mmmm"
 
+#define kFuncDirName   "CodeName"
+#define kPopupName     "API Selections"
+
+//------------------------------------------------------------------------------
+
 #if __INTEL__
 	extern "C" _EXPORT long perform_edit(MTextAddOn *addon);
 #else
@@ -44,6 +51,88 @@ Anyway... its handy.
 	}
 	#pragma export reset
 #endif
+
+//-----------------------------------------------------------------------------
+
+// Function Prototypes
+uint32	FetchQuery(query_op Op, const char* Selection, vector<BString>& Results, bool CaseSensitive = true);
+void	DisplayResults(vector<BString>& Results, MTextAddOn *addon);
+BString	FormatResultString(const BString& ResultPath);
+void	PopUpSelection(vector<BString>& Results, MTextAddOn *addon);
+
+//-----------------------------------------------------------------------------
+
+long perform_edit(MTextAddOn *addon)
+{
+	long selStart;
+	long selEnd;
+
+	addon->GetSelection(&selStart, &selEnd);
+
+	if (selEnd <= selStart)
+		return B_ERROR;
+
+	int length = selEnd - selStart;
+
+	BString selection;
+	selection.SetTo(addon->Text() + selStart, length);
+
+    // ... to avoid hanging Pe if we are called with long selections.
+	int pos = 0;
+	while ((pos < length) || pos < 30) // Hope 30 is ok.
+	{
+		if (!isalnum(selection[pos]) && (selection[pos] != '_')) break;
+		pos++;
+	}
+
+	if (pos > 0)
+		selection.Truncate(pos);
+	else
+		return B_ERROR;
+
+	vector <BString> results;
+
+	if (FetchQuery(B_EQ, selection.String(), results) > 0)
+	{
+		DisplayResults(results, addon);
+	}
+	else
+	{
+		// Lets try the same selection as a function name ...
+		BString funcname(selection.String());
+		funcname += "()";
+
+		if (FetchQuery(B_EQ, funcname.String(), results) > 0)
+		{
+			DisplayResults(results, addon);
+		}
+		else
+		{
+			// Still nothing? Lets Slap a B on the front and check for Classes!
+			BString section("B");
+			section += selection.String();
+
+			if (FetchQuery(B_EQ, section.String(), results, false) > 0)
+			{
+				DisplayResults(results, addon);
+			}
+			else
+			{
+				if (FetchQuery(B_CONTAINS, selection.String(), results, false) > 0)
+				{
+					DisplayResults(results, addon);
+				}
+				else
+				{
+					BString message(kNoMatch);
+					message += selection.String();
+					(new BAlert(kAlertName, message.String(), kBtnText))->Go(NULL);
+				}
+			}
+		}
+	}
+	return B_NO_ERROR;
+}
 
 //-----------------------------------------------------------------------------
 
@@ -93,98 +182,76 @@ uint32 FetchQuery(query_op Op, const char* Selection, vector<BString>& Results,
 
 //-----------------------------------------------------------------------------
 
-void DisplayResults(vector<BString>& Results)
+void DisplayResults(vector<BString>& Results, MTextAddOn *addon)
 {
-// For now just open the first one. Anyone wants to add a window to show results
-// and allow the selection of the html to show (as BeIDE does)?
+	if (Results.size() < 2)
+	{
+		char* arg = (char *)Results[0].String();
 
-//	if (Results.size() < 2)
-//	{
-		char* arg = (char *)Results[0].String(); 
-
-		// This one has one issue: if you happen to have another Tracker exec
-		// liying around on your boot volume... it will get executed instead of
-		// just call the already-running-Tracker's ArgRecv.
+		// This one has an issue: if you happen to have another Tracker exec
+		// somewhere on your boot volume... it will get executed instead of
+		// just calling the already-running-Tracker's ArgRecv method.
 		be_roster->Launch(kTrackerSig, 1, &arg);
-//	}
-//	else
-//	{
-
-//	}
+	}
+	else
+	{
+		PopUpSelection(Results, addon);
+	}
 }
 
 //-----------------------------------------------------------------------------
 
-long perform_edit(MTextAddOn *addon)
+BString FormatResultString(const BString& ResultPath)
 {
-	long selStart;
-	long selEnd;
+	BString result;
 
-	addon->GetSelection(&selStart, &selEnd);
-	if (selEnd <= selStart)
-		return B_ERROR; // should we BAlert the user about the empty selection?
+	int32 end = ResultPath.FindFirst(kFuncDirName) - 2;
+	int32 start = ResultPath.FindLast('/', end) + 1;
 
-	int length = selEnd - selStart;
-
-	// Crappy code follows...
-	BString selection;
-	selection.SetTo(addon->Text() + selStart, length);
-
-    // ... to avoid hanging Pe if we are called with long selections.
-	int pos = 0;
-	while ((pos < length) || pos < 30) // Hope 30 is ok.
+	if (end - start > 1)
 	{
-		if (!isalnum(selection[pos]) && (selection[pos] != '_')) break;
-		pos++;
-	}
-
-	if (pos > 0)
-		selection.Truncate(pos);
-	else
-		return B_ERROR;
-
-	vector <BString> results;
-
-	if (FetchQuery(B_EQ, selection.String(), results) > 0)
-	{
-		DisplayResults(results);
+		ResultPath.CopyInto(result, start, end - start + 1);
+		result += "::";
+		result += &ResultPath.String()[ResultPath.FindLast('/', ResultPath.Length()) + 1];
 	}
 	else
 	{
-		// Lets try the same selection as a function name ...
-		BString funcname(selection.String());
-		funcname += "()";
+		int32 secbegin = ResultPath.FindLast('/', ResultPath.Length()) + 1;
 
-		if (FetchQuery(B_EQ, funcname.String(), results) > 0)
-		{
-			DisplayResults(results);
-		}
-		else
-		{
-			// Still nothing? Lets Slap a B on the front and check for Classes!
-			BString section("B");
-			section += selection.String();
-
-			if (FetchQuery(B_EQ, section.String(), results, false) > 0)
-			{
-				DisplayResults(results);
-			}			
-			else
-			{
-				if (FetchQuery(B_CONTAINS, selection.String(), results, false) > 0)
-				{
-					DisplayResults(results);
-				}				
-				else
-				{
-					BString message(kNoMatch);
-					message += selection.String();
-					(new BAlert(kAlertName, message.String(), kBtnText))->Go(NULL);
-				}
-			}
-		}
+		ResultPath.CopyInto(result, secbegin, ResultPath.Length() - secbegin);
 	}
-	return B_NO_ERROR;
+
+	return result;
 }
 
-//-----------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+void PopUpSelection(vector<BString>& Results, MTextAddOn *addon)
+{
+	BPopUpMenu selections(kPopupName, false, false);
+	selections.SetFont(be_plain_font);
+
+	for (uint32 i = 0; i < Results.size(); i++)
+	{
+		BMenuItem* item = new BMenuItem(FormatResultString(Results[i]).String(), new BMessage(i));
+		selections.AddItem(item);
+	}
+
+	// Gotta love C++ syntaxis, sight... Anyway, here we get a nice BPoint to
+	// popup the results.
+	float x = (dynamic_cast<PDoc*>(addon->Window()))->ButtonBar()->Frame().right + 2;
+	float y = (dynamic_cast<PDoc*>(addon->Window()))->ToolBar()->Frame().Height() - 2;
+
+	BPoint menupos = addon->Window()->Frame().LeftTop() + BPoint(x, y);
+
+	BMenuItem* item = selections.Go(menupos, false, true);
+
+	if (NULL != item)
+	{
+		int index = item->Command();
+		char* arg = (char *)Results[index].String();
+		be_roster->Launch(kTrackerSig, 1, &arg);
+	}
+}
+
+//------------------------------------------------------------------------------
