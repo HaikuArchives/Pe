918c7091cbb8ce7d211960935e829010a76f97df
Oliver Tappe <zooey@hirschkaefer.de>
2004-03-31 09:02:18 +0000

    - established R4-compatibility (with a weird HACK).
    --HG--
    extra : convert_revision : svn%3Acdae58ac-7104-0410-addd-a5e18705bff5/trunk%40123

--- Sources/HAppResFile.h
+++ Sources/HAppResFile.h
@@ -38,6 +38,16 @@
 
 namespace HResources {
 
+#if (B_BEOS_VERSION <= B_BEOS_VERSION_4)
+// UGLY HACK WARNING!
+// [zooey]:
+// On BeOS R4, the first entry of this namespace - GetMenuBar() -
+// is magically renamed into GetMenuBar_() by gcc. Why this is the
+// case, I do not know. If anyone *does* know, please tell me!
+// We fix (cough) this by introducing a dummy entry:
+	void R4_Dummy();
+#endif
+
 BMenuBar* GetMenuBar(BRect frame, int id);
 BMenu* GetMenu(int id, bool popup = false);
 
