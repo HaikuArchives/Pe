b58eb22e9cb8caff0a84e0bafb8dca1fded5b02e
Oliver Tappe <zooey@hirschkaefer.de>
2004-04-12 17:24:08 +0000

    - applied Ingo Weinhold's patch that adds missing 'export' keyword to the   C++-language.
    --HG--
    extra : convert_revision : svn%3Acdae58ac-7104-0410-addd-a5e18705bff5/trunk%40130

--- Languages/Resources/keywords.cpp
+++ Languages/Resources/keywords.cpp
@@ -1,7 +1,7 @@
 #define #else #elif #endif #error #if #ifdef #ifndef #include #pragma
 #undef asm auto bool break case catch char class const const_cast 
-continue default delete do double dynamic_cast else enum extern false float 
-for friend goto if inherited inline int long mutable namespace new 
+continue default delete do double dynamic_cast else enum export extern false
+float for friend goto if inherited inline int long mutable namespace new 
 operator private protected public register reinterpret_cast return short 
 signed sizeof static static_cast struct switch template this throw 
 true try typedef typeid typename union unsigned using virtual void 
