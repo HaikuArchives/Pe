d86a2260350a601a62d689b93b72ec2aa9bb1716
Oscar Lesta <oscar.lesta@gmail.com>
2005-12-13 21:56:00 +0000

    - Uses MAlerts instead of BAlerts (ESC dismisses. Thanks Ingo). - Made the code/file shorter, style changes. - Less Broken-English (I hope).
    --HG--
    extra : convert_revision : svn%3Acdae58ac-7104-0410-addd-a5e18705bff5/trunk%40415

--- Extensions/BeBookFetch.cpp
+++ Extensions/BeBookFetch.cpp
@@ -1,4 +1,4 @@
-//-----------------------------------------------------------------------------
+//
 // BeBookFetch: looks up the selected text against BeBook's doc bookmarks.
 //
 // Contains (legally) stolen code from:
@@ -8,171 +8,26 @@
 //
 // ToDo: improve the popup display by reading doc_bookmark attributes to avoid
 //       duplicates items (append, for example, "be:kit_name" attr content).
-//-----------------------------------------------------------------------------
+//
 
-#include <vector>
-#include <ctype.h>
+#include "PeAddOn.h"
 
-#include <Alert.h>
-#include <AppDefs.h>
-#include <MenuItem.h>
-#include <Messenger.h>
-#include <Path.h>
-#include <PopUpMenu.h>
-#include <Query.h>
-#include <Roster.h>
-#include <String.h>
-#include <Volume.h>
-#include <VolumeRoster.h>
-
-#include <typeinfo> // otherwise I get an error in PDoc.h :-/
-#include <Debug.h>  // ditto.
-#include "PDoc.h"
-
-#include "MTextAddOn.h"
-#include "HButtonBar.h"
-#include "PToolBar.h"
+#define kTrackerSig		"application/x-vnd.Be-TRAK"
 
 //------------------------------------------------------------------------------
 
-#define kBeTypeAttrib  "BEOS:TYPE"
-#define kIndexFileType "application/x-vnd.Be-doc_bookmark"
-#define kName          "name"
-#define kFuncDirName   "CodeName"
-#define kTrackerSig    "application/x-vnd.Be-TRAK"
-#define kNoMatch       "Nothing was found for: "
-#define kAlertName     "Pe's BeAPI Fetcher Alert"
-#define kBtnText       "Mmmm"
-#define kPopupName     "API Selections"
-
-//------------------------------------------------------------------------------
-
-#if __INTEL__
-	extern "C" _EXPORT long perform_edit(MTextAddOn *addon);
-#else
-	#pragma export on
-	extern "C" {
-		long perform_edit(MTextAddOn *addon);
-	}
-	#pragma export reset
-#endif
-
-//-----------------------------------------------------------------------------
-
-// Function Prototypes
-uint32	FetchQuery(query_op Op, const char* Selection, vector<BString>& Results, bool CaseSensitive = false);
-void	DisplayResults(vector<BString>& Results, MTextAddOn *addon);
-BString	FormatResultString(const BString& ResultPath);
-void	PopUpSelection(vector<BString>& Results, MTextAddOn *addon);
-void	OpenDoc(char* arg);
-
-//-----------------------------------------------------------------------------
-
-long perform_edit(MTextAddOn *addon)
-{
-	long selStart;
-	long selEnd;
-
-	addon->GetSelection(&selStart, &selEnd);
-
-	if (selEnd <= selStart)
-		return B_ERROR;
-
-	int length = selEnd - selStart;
-
-	// Do _NOT_ Query if we have less than 3 chars. It works, but takes ages and
-	// yields to an unuseful giant popup menu.
-	if (length < 3)
-	{
-		(new BAlert("BeBookFetch", "The text selection is too short.\n"
-		"Please select three or more characters.", "Ok"))->Go(NULL);
-
-		return B_ERROR;
-	}
-
-	BString selection;
-	selection.SetTo(addon->Text() + selStart, length);
-
-    // ... to avoid hanging Pe if we are called with long selections.
-	int pos = 0;
-	while ((pos < length) || pos < 30) // Hope 30 is ok.
-	{
-		if (!isalnum(selection[pos]) && (selection[pos] != '_')) break;
-		pos++;
-	}
-
-	if (!pos) return B_ERROR;
-
-	selection.Truncate(pos);
-
-	// Do _NOT_ Query if we have less than 3 chars. It works, but takes ages and
-	// yields to an unuseful giant popup menu.
-	if (selection.CountChars() < 3)
-	{
-		(new BAlert("BeBookFetch", "The text selection is too short.\n"
-			"Make sure the selection doesn't contains puntuations or symbols.",
-			"Ok"))->Go(NULL);
-
-		return B_ERROR;
-	}
-
-	vector <BString> results;
-
-	if (FetchQuery(B_EQ, selection.String(), results) > 0)
-	{
-		DisplayResults(results, addon);
-	}
-	else
-	{
-		// Lets try the same selection as a function name ...
-		BString funcname(selection.String());
-		funcname += "()";
-
-		if (FetchQuery(B_EQ, funcname.String(), results) > 0)
-		{
-			DisplayResults(results, addon);
-		}
-		else
-		{
-			// Still nothing? Lets Slap a B on the front and check for Classes!
-			BString section("B");
-			section += selection.String();
-
-			if (FetchQuery(B_EQ, section.String(), results, false) > 0)
-			{
-				DisplayResults(results, addon);
-			}
-			else
-			{
-				if (FetchQuery(B_CONTAINS, selection.String(), results, false) > 0)
-				{
-					DisplayResults(results, addon);
-				}
-				else
-				{
-					BString message(kNoMatch);
-					message += selection.String();
-					(new BAlert(kAlertName, message.String(), kBtnText))->Go(NULL);
-				}
-			}
-		}
-	}
-	return B_NO_ERROR;
-}
-
-//-----------------------------------------------------------------------------
-
-uint32 FetchQuery(query_op Op, const char* Selection, vector<BString>& Results,
-                  bool CaseSensitive)
+uint32
+FetchQuery(query_op op, const char* selection, vector<BString>& results,
+			bool caseSensitive = false)
 {
 	BQuery query;
 
-	query.PushAttr(kBeTypeAttrib);
-	query.PushString(kIndexFileType);
+	query.PushAttr("BEOS:TYPE");
+	query.PushString("application/x-vnd.Be-doc_bookmark");
 	query.PushOp(B_EQ);
-	query.PushAttr(kName);
-	query.PushString(Selection, CaseSensitive);
-	query.PushOp(Op);
+	query.PushAttr("name");
+	query.PushString(selection, caseSensitive);
+	query.PushOp(op);
 	query.PushOp(B_AND);
 
 	BVolume vol;
@@ -182,48 +37,29 @@ uint32 FetchQuery(query_op Op, const char* Selection, vector<BString>& Results,
 	query.SetVolume(&vol);
 
 	if (B_NO_INIT == query.Fetch())
-	{
 		return 0;
-	}
 
 	BEntry entry;
 	BPath path;
 	int32 counter = 0;
 
-	while (query.GetNextEntry(&entry) != B_ENTRY_NOT_FOUND)
-	{
-		if (entry.InitCheck() == B_OK)
-		{
+	while (query.GetNextEntry(&entry) != B_ENTRY_NOT_FOUND) {
+		if (entry.InitCheck() == B_OK) {
 			entry.GetPath(&path);
 
-			if (path.InitCheck() == B_OK)
-			{
-				Results.push_back(path.Path());
+			if (path.InitCheck() == B_OK) {
+				results.push_back(path.Path());
 				counter++;
 			}
 		}
 	}
+
 	return counter;
 }
 
-//-----------------------------------------------------------------------------
 
-void DisplayResults(vector<BString>& Results, MTextAddOn *addon)
-{
-	if (Results.size() < 2)
-	{
-		char* arg = (char *)Results[0].String();
-		OpenDoc(arg);
-	}
-	else
-	{
-		PopUpSelection(Results, addon);
-	}
-}
-
-//-----------------------------------------------------------------------------
-
-void OpenDoc(char* arg)
+void
+OpenDoc(const char* fileName)
 {
 // Note:
 // I don't just call be_roster->Launch() here because:
@@ -233,53 +69,50 @@ void OpenDoc(char* arg)
 // takes too long to get the results :-)
 
 	BMessage msg(B_ARGV_RECEIVED);
-	msg.AddString("cwd", "/"); // OT's TTracker::ArgvReceived fault.
+	msg.AddString("cwd", "/");
 	msg.AddString("argv", "Tracker");
-	msg.AddString("argv", arg);
+	msg.AddString("argv", fileName);
 	msg.AddInt32("argc", 2);
 
 	BMessenger messenger(kTrackerSig);
 	if (messenger.IsValid())
 		messenger.SendMessage(&msg);
 	else
-		be_roster->Launch(kTrackerSig, 1, &arg);
+		be_roster->Launch(kTrackerSig, 1, &const_cast<char*>(fileName));
 }
 
-//-----------------------------------------------------------------------------
 
-BString FormatResultString(const BString& ResultPath)
+BString
+FormatResultString(const BString& resultPath)
 {
 	BString result;
 
-	int32 end = ResultPath.FindFirst(kFuncDirName) - 2;
-	int32 start = ResultPath.FindLast('/', end) + 1;
+	int32 end = resultPath.FindFirst("CodeName") - 2;
+	int32 start = resultPath.FindLast('/', end) + 1;
 
-	if (end - start > 1)
-	{
-		ResultPath.CopyInto(result, start, end - start + 1);
+	if (end - start > 1) {
+		resultPath.CopyInto(result, start, end - start + 1);
 		result += "::";
-		result += &ResultPath.String()[ResultPath.FindLast('/', ResultPath.Length()) + 1];
-	}
-	else
-	{
-		int32 secbegin = ResultPath.FindLast('/', ResultPath.Length()) + 1;
-
-		ResultPath.CopyInto(result, secbegin, ResultPath.Length() - secbegin);
+		result += &resultPath.String()[resultPath.FindLast('/',
+					resultPath.Length()) + 1];
+	} else {
+		int32 secbegin = resultPath.FindLast('/', resultPath.Length()) + 1;
+		resultPath.CopyInto(result, secbegin, resultPath.Length() - secbegin);
 	}
 
 	return result;
 }
 
-//------------------------------------------------------------------------------
 
-void PopUpSelection(vector<BString>& Results, MTextAddOn *addon)
+void
+PopUpResults(vector<BString>& results, MTextAddOn* addon)
 {
-	BPopUpMenu selections(kPopupName, false, false);
+	BPopUpMenu selections("API Selections", false, false);
 	selections.SetFont(be_plain_font);
 
-	for (uint32 i = 0; i < Results.size(); i++)
-	{
-		BMenuItem* item = new BMenuItem(FormatResultString(Results[i]).String(), new BMessage(i));
+	for (uint32 i = 0; i < results.size(); i++) {
+		BMenuItem* item = new BMenuItem(FormatResultString(results[i]).String(),
+										new BMessage(i));
 		selections.AddItem(item);
 	}
 
@@ -296,12 +129,114 @@ void PopUpSelection(vector<BString>& Results, MTextAddOn *addon)
 
 	BMenuItem* item = selections.Go(menupos, false, true);
 
-	if (item != NULL)
-	{
+	if (item != NULL) {
 		int index = item->Command();
-		char* arg = (char *)Results[index].String();
-		OpenDoc(arg);
+		OpenDoc(results[index].String());
 	}
 }
 
+
+void
+DisplayResults(vector<BString>& results, MTextAddOn* addon)
+{
+	if (results.size() < 2)
+		OpenDoc(results[0].String());
+	else
+		PopUpResults(results, addon);
+}
+
+
 //------------------------------------------------------------------------------
+//	#pragma mark - Addon Entry
+
+#if __INTEL__
+	extern "C" _EXPORT long perform_edit(MTextAddOn* addon);
+#else
+	#pragma export on
+	extern "C" {
+		long perform_edit(MTextAddOn* addon);
+	}
+	#pragma export reset
+#endif
+
+
+long
+perform_edit(MTextAddOn* addon)
+{
+	long selStart;
+	long selEnd;
+
+	addon->GetSelection(&selStart, &selEnd);
+
+	if (selEnd <= selStart)
+		return B_ERROR;
+
+	int length = selEnd - selStart;
+
+	// Do NOT run the query if we have less than 3 chars. It works, but takes
+	// ages and produces an useless giant popup menu.
+	if (length < 3) {
+		MIdeaAlert("The text selection is too short.\n"
+					"Please select at least three characters.").Go();
+		return B_ERROR;
+	}
+
+	BString selection;
+	selection.SetTo(addon->Text() + selStart, length);
+
+    // Limit the size of the search string to 30 chars, and make sure it is
+    // worth of searching for (name of: function/variable/error code, etc.).
+	int pos = 0;
+	while ((pos < length) || pos < 30) {
+		if (!isalnum(selection[pos]) && (selection[pos] != '_')) break;
+		pos++;
+	}
+
+	if (!pos) return B_ERROR;
+
+	selection.Truncate(pos);
+
+	if (selection.CountChars() < 3) {
+		MIdeaAlert("The text selection is too short.\n"
+					"Make sure that the selection doesn't contains punctuation "
+					"marks or other symbols.").Go();
+		return B_ERROR;
+	}
+
+	vector<BString> results;
+
+	if (FetchQuery(B_EQ, selection.String(), results) > 0) {
+		DisplayResults(results, addon);
+		return B_OK;
+	}
+
+	// No results... Lets try the same selection as a function name...
+	BString funcname(selection.String());
+	funcname += "()";
+
+	if (FetchQuery(B_EQ, funcname.String(), results) > 0) {
+		DisplayResults(results, addon);
+		return B_OK;
+	}
+
+	// Still nothing? Lets Slap a B on the front and check for Classes!
+	BString section("B");
+	section += selection.String();
+
+	if (FetchQuery(B_EQ, section.String(), results, false) > 0) {
+		DisplayResults(results, addon);
+		return B_OK;
+	}
+
+	// Last try... partial match.
+	if (FetchQuery(B_CONTAINS, selection.String(), results, false) > 0) {
+		DisplayResults(results, addon);
+		return B_OK;
+	}
+
+	BString message("Nothing was found for: ");
+	message += selection.String();
+	MInfoAlert(message.String()).Go();
+
+	return B_OK;
+}
