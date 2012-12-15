b06518a059b3b1d4513509b167f6d6ffe81a7a5f
Przemysław Buczkowski <przemub@yahoo.pl>
2012-12-15 02:00:02 +0100

    Add syntax highlighter for Ruby files (GCI work)

---
+++ Languages/Resources/keywords.rb
@@ -0,0 +1,18 @@
+-Pe-Keywords-Language-
+alias and BEGIN begin break case class def defined? do else elsif END
+end ensure false __FILE__ for if in __LINE__ module next nil not or redo
+rescue retry return self super then true undef unless until when while
+yield
+
+-Pe-Keywords-User1-
+
+-Pe-Keywords-User2-
+
+-Pe-Keywords-User3-
+
+-Pe-Keywords-Constant-
+$! $@ $& $` $' $+ $1, $2.. $~ $= $/ $\ $, $; $. $< $> $_ $0 $* $$ $? $:
+$" $DEBUG $FILENAME $LOAD_PATH $stdin $stdout $stderr $VERBOSE $-0 $-a 
+$-d $-F $-i $-I $-l $-p $-v
+
+
