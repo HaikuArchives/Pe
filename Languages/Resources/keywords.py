13ba47c6b167066165a1ee02915762c763bd8756
Oscar Lesta <oscar.lesta@gmail.com>
2023-09-10 16:51:13 -0300

    keywords.py: update for Python 3.10

--- Languages/Resources/keywords.py
+++ Languages/Resources/keywords.py
@@ -1,7 +1,69 @@
-access and break class continue def del elif else except exec 
-finally for from global if import in is lambda not or pass 
-print raise return try while
-- User set 1
-- User set 2
-- User set 3
+and as assert async await break class continue def del elif else
+except False finally for from global if import in is lambda None
+nonlocal not or pass raise return True try while with yield
+
+- User set 1 // built-in functions
+abs aiter all anext any ascii bin bool breakpoint bytearray bytes
+callable chr classmethod compile complex copyright credits delattr
+dict dir divmod enumerate eval exec exit filter float format frozenset
+getattr globals hasattr hash help hex id input int isinstance issubclass
+iter len license list locals map max memoryview min next object oct open
+ord pow print property quit range repr reversed round set setattr slice
+sorted staticmethod str sum super tuple type vars zip
+
+- User set 2 // Exceptions
+ArithmeticError AssertionError AttributeError BaseException
+BlockingIOError BrokenPipeError BufferError BytesWarning
+ChildProcessError ConnectionAbortedError ConnectionError
+ConnectionRefusedError ConnectionResetError DeprecationWarning
+EOFError Ellipsis EncodingWarning EnvironmentError Exception
+FileExistsError FileNotFoundError FloatingPointError FutureWarning
+GeneratorExit IOError ImportError ImportWarning IndentationError
+IndexError InterruptedError IsADirectoryError KeyError KeyboardInterrupt
+LookupError MemoryError ModuleNotFoundError NameError
+NotADirectoryError NotImplemented NotImplementedError OSError
+OverflowError PendingDeprecationWarning PermissionError
+ProcessLookupError RecursionError ReferenceError ResourceWarning
+RuntimeError RuntimeWarning StopAsyncIteration StopIteration
+SyntaxError SyntaxWarning SystemError SystemExit TabError TimeoutError
+TypeError UnboundLocalError UnicodeDecodeError UnicodeEncodeError
+UnicodeError UnicodeTranslateError UnicodeWarning UserWarning
+ValueError Warning ZeroDivisionError
+
+- User set 3 // modules from stdlib
+__future__ _abc _aix_support _ast _asyncio _bisect _blake2 _bootsubprocess
+_bz2 _codecs _codecs_cn _codecs_hk _codecs_iso2022 _codecs_jp _codecs_kr
+_codecs_tw _collections _collections_abc _compat_pickle _compression
+_contextvars _crypt _csv _ctypes _curses _curses_panel _datetime _dbm
+_decimal _elementtree _frozen_importlib _frozen_importlib_external _functools
+_gdbm _hashlib _heapq _imp _io _json _locale _lsprof _lzma _markupbase _md5
+_msi _multibytecodec _multiprocessing _opcode _operator _osx_support
+_overlapped _pickle _posixshmem _posixsubprocess _py_abc _pydecimal _pyio
+_queue _random _scproxy _sha1 _sha256 _sha3 _sha512 _signal _sitebuiltins
+_socket _sqlite3 _sre _ssl _stat _statistics _string _strptime _struct
+_symtable _thread _threading_local _tkinter _tracemalloc _uuid _warnings
+_weakref _weakrefset _winapi _zoneinfo abc aifc antigravity argparse array
+ast asynchat asyncio asyncore atexit audioop base64 bdb binascii binhex
+bisect builtins bz2 cProfile calendar cgi cgitb chunk cmath cmd code codecs
+codeop collections colorsys compileall concurrent configparser contextlib
+contextvars copy copyreg crypt csv ctypes curses dataclasses datetime dbm
+decimal difflib dis distutils doctest email encodings ensurepip enum errno
+faulthandler fcntl filecmp fileinput fnmatch fractions ftplib functools gc
+genericpath getopt getpass gettext glob graphlib grp gzip hashlib heapq
+hmac html http idlelib imaplib imghdr imp importlib inspect io ipaddress
+itertools json keyword lib2to3 linecache locale logging lzma mailbox mailcap
+marshal math mimetypes mmap modulefinder msilib msvcrt multiprocessing netrc
+nis nntplib nt ntpath nturl2path numbers opcode operator optparse os
+ossaudiodev pathlib pdb pickle pickletools pipes pkgutil platform plistlib
+poplib posix posixpath pprint profile pstats pty pwd py_compile pyclbr
+pydoc pydoc_data pyexpat queue quopri random re readline reprlib resource
+rlcompleter runpy sched secrets select selectors shelve shlex shutil signal
+site smtpd smtplib sndhdr socket socketserver spwd sqlite3 sre_compile
+sre_constants sre_parse ssl stat statistics string stringprep struct
+subprocess sunau symtable sys sysconfig syslog tabnanny tarfile telnetlib
+tempfile termios textwrap this threading time timeit tkinter token tokenize
+trace traceback tracemalloc tty turtle turtledemo types typing unicodedata
+unittest urllib uu uuid venv warnings wave weakref webbrowser winreg
+winsound wsgiref xdrlib xml xmlrpc zipapp zipfile zipimport zlib zoneinfo
+
 - User set 4
